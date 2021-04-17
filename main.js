4314aa23a90fd6db0038e5a0367e1d84345eda98
viewpointicstan <viewpointicstan@gmail.com>
2021-04-17 18:39:06 +0900

    update: project description

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import init, { run_app } from './pkg/yew_wasm_pack_tailwind.js';
+import init, { run_app } from './pkg/piano_with_yew.js';
 import "./css/styles.css";
 
 async function main() {
-   await init('/pkg/yew_wasm_pack_tailwind_bg.wasm');
+   await init('/pkg/piano_with_yew_bg.wasm');
    run_app();
 }
 main()
